47dd0134276c7d13b05ac15cc0be863649c08528
vickbulba <victorbulbarelli@gmail.com>
2023-01-25 11:12:43 -0300

    adicionado HOME com icon (wip)

--- src/router/index.js
+++ src/router/index.js
@@ -5,21 +5,20 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    meta: {
+      title: 'home',
+      icon: "house",
+    },
+    component: {
+      HomeView
+    },
   },
   {
     path: '/login',
     name: 'login',
     component: () => import('../views/login/login')
-  }
+  },
+
 ]
 
 const router = createRouter({
