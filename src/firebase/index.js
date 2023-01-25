5ee80cb313f76010885d53d0a09621410bcba945
Kaue Depresbiteris <kaue.sobral19@gmail.com>
2023-01-24 23:49:33 -0300

    implementação do firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDefm1o7AR5vXmh_hCjo_95hP1TfCZSpYQ",
+  authDomain: "devtech-4d394.firebaseapp.com",
+  projectId: "devtech-4d394",
+  storageBucket: "devtech-4d394.appspot.com",
+  messagingSenderId: "47580555547",
+  appId: "1:47580555547:web:31a536fe7347283d40cbbe",
+  measurementId: "G-7JME259PKC"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export default db
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import FirebaseVue from './firebase'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$firebase = FirebaseVue
+app.use(router).mount('#app')
