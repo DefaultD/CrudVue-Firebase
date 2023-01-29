290486c6a18c0aad46ba547d20a9b7886e6dd4de
Kaue Depresbiteris <kaue.sobral19@gmail.com>
2023-01-29 01:10:20 -0300

    tela de clientes em progresso

--- src/components/global/baseSpinner.vue
+++ src/components/global/baseSpinner.vue
@@ -1,30 +1,30 @@
 <template>
     <div class="base-spinner fa-3x" v-if="visible">
-      <i class="fas fa-circle-notch fa-spin"></i>
+        <i class="fas fa-circle-notch fa-spin"></i>
     </div>
-  </template>
+</template>
 
-  <script>
-  export default {
+<script>
+export default {
     data() {
-    return {
-      loading: true,
-      isLogged: false,
-      visible: true,
-    }
-  },
+        return {
+            loading: true,
+            isLogged: false,
+            visible: true,
+        }
+    },
 
     props: {
-    
+
     },
-    created () {
-    this.$root.$watch('baseSpinner::show', () => { this.visible = true })
-    this.$root.$watch('baseSpinner::hide', () => { this.visible = false })
-  },
-  }
-  </script>
-  <style lang="scss" scoped>
-  .base-spinner {
+    created() {
+        this.$root.$watch('baseSpinner::show', () => { this.visible = true })
+        this.$root.$watch('baseSpinner::hide', () => { this.visible = false })
+    },
+}
+</script>
+<style lang="scss" scoped>
+.base-spinner {
     top: 0;
     bottom: 0;
     right: 0;
@@ -38,6 +38,5 @@
     background-color: var(--dark-medium);
     color: var(--featured);
     z-index: 10000000;
-  }
-  </style>
-  
+}
+</style>
---
+++ src/views/customer/viewCustomers.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="row justify-content-between">
+        <div class="col">
+            <h3>Clientes</h3>
+        </div>
+        <layoutCreateNewClient  />
+    </div>
+    <hr/>
+    <div class="mt-2 row">
+        <div :key="customer" v-for="customer of customersData" class="my-3 col-lg-4 rounded">
+        <infoCard :data="customer"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import layoutCreateNewClient from '@/components/layout/layoutCreateNewClient.vue'
+import infoCard from '@/components/global/infoCard.vue'
+
+export default {
+    name: "viewCustomer",
+
+    data() {
+        return{
+            customersData: [
+                {
+                    name: 'Kaue Depresbiteris',
+                    type: 'Pf',
+                    id: '9384',
+                    createdAt: '29/01/2023', //colocar
+                    email: 'Kaue.sobral19@gmail.com',
+                    phone: '(11) 95486-6282',
+                    Address: 'Rua solidonio leite 2718',
+                    birthDate: '19/06/2000',
+                    complement: '',
+                    description: ''
+                },
+                {
+                    name: 'Kaue Depresbiteris',
+                    type: 'Pf',
+                    id: '9384',
+                    createdAt: '29/01/2023',
+                    email: 'Kaue.sobral19@gmail.com',
+                    phone: '(11) 95486-6282',
+                    birthDate: '19/06/2000',
+                    Address: 'Rua solidonio leite 2718',
+                    complement: 'Bloco 3 ap 151',
+                    description: ''
+                },
+                {
+                    name: 'Kaue Depresbiteris',
+                    type: 'Pf',
+                    id: '9384',
+                    createdAt: '29/01/2023',
+                    email: 'Kaue.sobral19@gmail.com',
+                    phone: '(11) 95486-6282',
+                    birthDate: '19/06/2000',
+                    Address: 'Rua solidonio leite 2718',
+                    complement: 'Bloco 3 ap 151',
+                    description: ''
+                },
+                {
+                    name: 'Kaue Depresbiteris',
+                    type: 'Pf',
+                    id: '9384',
+                    createdAt: '29/01/2023',
+                    email: 'Kaue.sobral19@gmail.com',
+                    phone: '(11) 95486-6282',
+                    birthDate: '19/06/2000',
+                    Address: 'Rua solidonio leite 2718',
+                    complement: 'Bloco 3 ap 151',
+                    description: ''
+                }
+            ]
+        }
+    },
+
+    components: {
+        layoutCreateNewClient,
+        infoCard
+    },
+
+}
+
+</script>
